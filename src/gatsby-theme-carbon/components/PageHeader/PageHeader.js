829cba9ff51dd128300a4e6ad388525e8aedb0f8
DKL <dkl@zurich.ibm.com>
2022-06-15 09:08:20 +0200

    Add documents parsed counter.
    Signed-off-by: DKL <dkl@zurich.ibm.com>

--- src/gatsby-theme-carbon/components/PageHeader/PageHeader.js
+++ src/gatsby-theme-carbon/components/PageHeader/PageHeader.js
@@ -1,25 +1,65 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { useEffect, useState } from "react";
 import cx from "classnames";
 import * as styles from "./PageHeader.module.scss";
 
-const CustomPageHeader = ({ title, theme, tabs = [] }) => (
-  <div
-    className={cx(styles.pageHeader, {
-      [styles.withTabs]: tabs.length,
-      [styles.darkMode]: theme === "dark",
-    })}
-  >
-    <div className="bx--grid">
-      <div className="bx--row">
-        <div className="bx--col-lg-12">
-          <h1 id="page-title" className={styles.text}>
-            {title.length > 0 ? title : "Deep Search"}
-          </h1>
+const documentsConvertedUrl = "/documentsConverted.json";
+
+const CustomPageHeader = ({ title, theme, tabs = [] }) => {
+  const isHomePage = title.length === 0;
+
+  const [documentsConverted, setDocumentsConverted] = useState();
+
+  // Update converted documents each minute.
+  useEffect(() => {
+    let interval;
+
+    if (isHomePage) {
+      interval = setInterval(updateDocumentsConverted, 60000);
+      updateDocumentsConverted();
+    }
+
+    function updateDocumentsConverted() {
+      try {
+        fetch(documentsConvertedUrl)
+          .then((response) => response.json())
+          .then(({ documentsConverted }) =>
+            setDocumentsConverted(documentsConverted)
+          );
+      } catch (ex) {
+        console.error("Failed to fetch converted documents:", ex);
+      }
+    }
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div
+      className={cx(styles.pageHeader, {
+        [styles.withTabs]: tabs.length,
+        [styles.darkMode]: theme === "dark",
+      })}
+    >
+      <div className="bx--grid">
+        <div className="bx--row">
+          <div className="bx--col-lg-8">
+            <h1 id="page-title" className={styles.text}>
+              {isHomePage ? "Deep Search" : title}
+            </h1>
+          </div>
+
+          {documentsConverted > 0 && (
+            <div className={`bx--col-lg-4 ${styles.documentCounter}`}>
+              <span className={styles.documentCount}>
+                {documentsConverted.toLocaleString()}
+              </span>
+              <span>documents parsed</span>
+            </div>
+          )}
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default CustomPageHeader;
