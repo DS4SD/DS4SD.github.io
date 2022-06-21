e6e9dfca50104675ba1863c36171325df6362c93
DKL <dkl@zurich.ibm.com>
2022-06-21 10:30:59 +0200

    Fix small screen layout and navigation.
    Signed-off-by: DKL <dkl@zurich.ibm.com>

--- src/gatsby-theme-carbon/components/Header/index.js
+++ src/gatsby-theme-carbon/components/Header/index.js
@@ -1,35 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Header as ShellHeader,
+  HeaderMenuButton,
   HeaderName,
   HeaderGlobalBar,
   Button,
 } from "carbon-components-react";
 import { Email20, ArrowRight20 } from "@carbon/icons-react";
+import LeftNav from "./LeftNav";
+import * as styles from "./styles.module.scss";
 
-const CustomHeader = ({}) => {
+const CustomHeader = () => {
+  const [navExpanded, setNavExpanded] = useState(false);
+
   return (
     <ShellHeader aria-label="Header">
-      <HeaderName href="#" prefix="IBM">
-        Deep Search
-      </HeaderName>
+      <HeaderMenuButton
+        aria-label="Open navigation"
+        onClick={() => setNavExpanded(!navExpanded)}
+        isActive={navExpanded}
+      />
+
+      <HeaderName prefix="IBM">Deep Search</HeaderName>
 
       <HeaderGlobalBar>
         <Button
+          className={styles.navButton}
           kind="secondary"
           renderIcon={Email20}
           href="mailto:deepsearch-core@zurich.ibm.com"
         >
-          Contact us
+          <span>Contact us</span>
         </Button>
 
         <Button
+          className={styles.navButton}
           renderIcon={ArrowRight20}
           href="https://deepsearch-experience.res.ibm.com"
         >
-          Try Deep Search
+          <span>Try Deep Search</span>
         </Button>
       </HeaderGlobalBar>
+
+      <LeftNav expanded={navExpanded} />
     </ShellHeader>
   );
 };
---
+++ src/gatsby-theme-carbon/components/LeftNav/LeftNav.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+// Left nav has been moved to the header, for expand and collapse behavior.
+const LeftNav = () => null;
+
+export default LeftNav;
